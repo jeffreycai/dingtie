666c597ccea5fd578fbfe465265dce111bb417fd
jeffreycai <jeffreycaizhenyuan@gmail.com>
2015-05-21 14:51:19 +1000

    minor

--- modules/site/cron/tigtag_inc.random.php
+++ modules/site/cron/tigtag_inc.random.php
@@ -15,144 +15,146 @@ $settings = Vars::getSettings();
 $messages = $settings['dingtie_messages'];
 
 
-// get a valid user
-$user;
-if ($user = TigtagUser::getFirstValidUserWithMaxRank(150)) {
+for ($counter = 0; $counter < 5; $counter++) {
+            // get a valid user
+            $user;
+            if ($user = TigtagUser::getFirstValidUserWithMaxRank(150)) {
 
-  $user->setLastUsed(time());
-  $user->save();
-  
-  // first request to get formhash
-  $crawler = new Crawler();
-  $crawler->setCookiePath($user->getCookiePath());
-//  $crawler->setUseTor();
-  $crawler->setHeader(array(
-      'Referer: http://bbs.tigtag.com/forum.php?mod=viewthread&tid=3160720&pid=31999233',
-  ));
-  $url = "http://bbs.tigtag.com/forum.php?mod=post&action=reply&fid=$fid&tid=$tid";
-  
-  $html = $crawler->read($url);
-  $matches = array();
-  preg_match('/id="formhash" value="([^"]+)"/', $html, $matches);
-  $formhash = isset($matches[1]) ? $matches[1] : null;
+              $user->setLastUsed(time());
+              $user->save();
 
-  // Check if we need to do an extra secqaa verification
-  $secquestion;
-  $secanswer;
-  $sechash;
-  if (preg_match('/id="secqaaverify_([^"]+)"/', $html, $matches)) {
-    $secqaa_code = $matches[1];
-    $crawler->setHeader(array(
-        'Referer: ' . $url,
-    ));
-    
-    // do math seccode verification, try for 5 times
-    $secverification_success = false;
-    for ($i = 0; $i < 7; $i++) {
-$html = $crawler->read("http://bbs.tigtag.com/misc.php?mod=secqaa&action=update&idhash=$secqaa_code&inajax=1&ajaxtarget=secqaa_$secqaa_code");
-      $html = iconv('GBK', 'UTF-8', $html);
-      $secquestion = $html;
-  //die($html);
-      if (preg_match('/(\d+) ?([\+\-*\/]) ?(\d+) ?= ?/', $html, $matches)) {
-        $first = intval($matches[1]);
-        $operand = $matches[2];
-        $second = $matches[3];
-        $result;
-        if ($operand == '+') {
-          $result = $first + $second;
-        } else if ($operand == '-') {
-          $result = $first - $second;
-        } else if ($operand == '*') {
-          $result = $first * $second;
-        } else if ($operand == '/') {
-          $result = $first / $second;
-        }
+              // first request to get formhash
+              $crawler = new Crawler();
+              $crawler->setCookiePath($user->getCookiePath());
+            //  $crawler->setUseTor();
+              $crawler->setHeader(array(
+                  'Referer: http://bbs.tigtag.com/forum.php?mod=viewthread&tid=3160720&pid=31999233',
+              ));
+              $url = "http://bbs.tigtag.com/forum.php?mod=post&action=reply&fid=$fid&tid=$tid";
 
-        if (!preg_match('/\-?\d+/', $result)) {
-          $msg = 'Error when doing secqaa code verification. $html = ' . htmlentities($html);
-          $log = new Log('cron', Log::ERROR, $msg);
-          $log->save();
-          sendemailAdmin('Dingtie: Error', $msg);
-        } else {
-          $secanswer = $result;
-          $sechash = $secqaa_code;
-        }
-        $html = $crawler->read("http://bbs.tigtag.com/misc.php?mod=secqaa&action=check&inajax=1&&idhash=$sechash&secverify=$secanswer");
+              $html = $crawler->read($url);
+              $matches = array();
+              preg_match('/id="formhash" value="([^"]+)"/', $html, $matches);
+              $formhash = isset($matches[1]) ? $matches[1] : null;
 
-        if (strpos($html, 'succeed')) {
-          $secverification_success = true;
-          break;
-        } else {
-          continue;
-        }
-      }
-    } // end for
-    if ($secverification_success == false) {
-      echo "Error - math verification failed.\n";
-      sendemailAdmin('Dingtie: Error - math verification failed. ', 'Tried 7 times but still failed');
-      exit;
-    }
-  }
+              // Check if we need to do an extra secqaa verification
+              $secquestion;
+              $secanswer;
+              $sechash;
+              if (preg_match('/id="secqaaverify_([^"]+)"/', $html, $matches)) {
+                $secqaa_code = $matches[1];
+                $crawler->setHeader(array(
+                    'Referer: ' . $url,
+                ));
 
-  if (is_null($formhash)) {
-    echo 'Error when getting form hash.';
-  }
+                // do math seccode verification, try for 5 times
+                $secverification_success = false;
+                for ($i = 0; $i < 7; $i++) {
+            $html = $crawler->read("http://bbs.tigtag.com/misc.php?mod=secqaa&action=update&idhash=$secqaa_code&inajax=1&ajaxtarget=secqaa_$secqaa_code");
+                  $html = iconv('GBK', 'UTF-8', $html);
+                  $secquestion = $html;
+              //die($html);
+                  if (preg_match('/(\d+) ?([\+\-*\/]) ?(\d+) ?= ?/', $html, $matches)) {
+                    $first = intval($matches[1]);
+                    $operand = $matches[2];
+                    $second = $matches[3];
+                    $result;
+                    if ($operand == '+') {
+                      $result = $first + $second;
+                    } else if ($operand == '-') {
+                      $result = $first - $second;
+                    } else if ($operand == '*') {
+                      $result = $first * $second;
+                    } else if ($operand == '/') {
+                      $result = $first / $second;
+                    }
 
-  // Second request to Post the comment
-//  $url_post = "http://bbs.tigtag.com/forum.php?mod=post&action=reply&fid=$fid&tid=$tid&extra=&replysubmit=yes";
-  $url_post = "http://bbs.tigtag.com/forum.php?mod=post&action=reply&fid=$fid&tid=$tid&extra=&replysubmit=yes&infloat=yes&handlekey=fastpost&inajax=1";
+                    if (!preg_match('/\-?\d+/', $result)) {
+                      $msg = 'Error when doing secqaa code verification. $html = ' . htmlentities($html);
+                      $log = new Log('cron', Log::ERROR, $msg);
+                      $log->save();
+                      sendemailAdmin('Dingtie: Error', $msg);
+                    } else {
+                      $secanswer = $result;
+                      $sechash = $secqaa_code;
+                    }
+                    $html = $crawler->read("http://bbs.tigtag.com/misc.php?mod=secqaa&action=check&inajax=1&&idhash=$sechash&secverify=$secanswer");
 
-  
-  if (isset($alternative_message)) {
-    $message = $alternative_message;
-  } else {
-    $message = $messages[array_rand($messages)];
-  }
-  $message = urlencode(iconv('UTF-8', 'GBK', $message));
-  
-  $data = "message=$message".( (isset($secanswer) && isset($sechash)) ? "&sechash=$sechash&secanswer=$secanswer" : '')."&posttime=$time&formhash=$formhash&usesig=1&subject=++";
+                    if (strpos($html, 'succeed')) {
+                      $secverification_success = true;
+                      break;
+                    } else {
+                      continue;
+                    }
+                  }
+                } // end for
+                if ($secverification_success == false) {
+                  echo "Error - math verification failed.\n";
+                  sendemailAdmin('Dingtie: Error - math verification failed. ', 'Tried 7 times but still failed');
+                  exit;
+                }
+              }
 
-  $crawler->setHeader(array(
-      'Referer: http://bbs.tigtag.com/thread-' . $tid . '-1-1.html',
-  ));
-// die("\n\n" . $data . "\n\n");
+              if (is_null($formhash)) {
+                echo 'Error when getting form hash.';
+              }
 
-  $html = $crawler->post($url_post, $data);
+              // Second request to Post the comment
+            //  $url_post = "http://bbs.tigtag.com/forum.php?mod=post&action=reply&fid=$fid&tid=$tid&extra=&replysubmit=yes";
+              $url_post = "http://bbs.tigtag.com/forum.php?mod=post&action=reply&fid=$fid&tid=$tid&extra=&replysubmit=yes&infloat=yes&handlekey=fastpost&inajax=1";
 
-//  _debug($html);
-  if (strpos($html, 'succeedhandle_fastpost')) {
-    
-    if (strstr(iconv('GBK', 'UTF-8', $html), '审核')) {
-      $msg = 'Notice when doing post replay for user #' . $user->getId() . ' - ' . $user->getUsername() . ' == ' . iconv('GBK', 'UTF-8', $html);
-//      $log = new Log('cron', Log::NOTICE, $msg);
-//      $log->save();
-      
-      echo "= Notice: Tigtag requires review.\n\n";
-      sendemailAdmin('Dingtie: Notice - requires review', $msg);
-    }
-    
-    echo "Success!\n";
-    echo iconv('GBK', 'UTF-8', $html);
-    echo "\n";
-    
-    $user->refreshRank();
-    
-  } else {
-    $msg = 'Error when doing post reply. User is #' . $user->getId() . ' - ' . $user->getUsername() . ' == ' . (isset($secanswer) ? "<p>Secquestion: ".htmlentities($secquestion)."<br />Secanswer: $secanswer</p>" : "") . (iconv('GBK', 'UTF-8', $html) ? iconv('GBK', 'UTF-8', $html) : $html);
-//    $log = new Log('cron', Log::ERROR, $msg);
-//    $log->save();
-    
-    echo "Failed.. \n";
-    echo (iconv('GBK', 'UTF-8', $html) ? iconv('GBK', 'UTF-8', $html) : $html);
-    echo "\n";
-    sendemailAdmin('Dingtie: Error', $msg);
-  }
-} else {
-  $msg = 'Error when doing post reply. no valid user available';
-//  $log = new Log('cron', Log::ERROR, $msg);
-//  $log->save();
-  
-  echo "Error: no valid user available";
-  echo "\n";
-  sendemailAdmin('Dingtie: Error', $msg);
+
+              if (isset($alternative_message)) {
+                $message = $alternative_message;
+              } else {
+                $message = $messages[array_rand($messages)];
+              }
+              $message = urlencode(iconv('UTF-8', 'GBK', $message));
+
+              $data = "message=$message".( (isset($secanswer) && isset($sechash)) ? "&sechash=$sechash&secanswer=$secanswer" : '')."&posttime=$time&formhash=$formhash&usesig=1&subject=++";
+
+              $crawler->setHeader(array(
+                  'Referer: http://bbs.tigtag.com/thread-' . $tid . '-1-1.html',
+              ));
+            // die("\n\n" . $data . "\n\n");
+
+              $html = $crawler->post($url_post, $data);
+
+            //  _debug($html);
+              if (strpos($html, 'succeedhandle_fastpost')) {
+
+                if (strstr(iconv('GBK', 'UTF-8', $html), '审核')) {
+                  $msg = 'Notice when doing post replay for user #' . $user->getId() . ' - ' . $user->getUsername() . ' == ' . iconv('GBK', 'UTF-8', $html);
+            //      $log = new Log('cron', Log::NOTICE, $msg);
+            //      $log->save();
+
+                  echo "= Notice: Tigtag requires review.\n\n";
+                  sendemailAdmin('Dingtie: Notice - requires review', $msg);
+                }
+
+                echo "Success!\n";
+                echo iconv('GBK', 'UTF-8', $html);
+                echo "\n";
+
+                $user->refreshRank();
+
+              } else {
+                $msg = 'Error when doing post reply. User is #' . $user->getId() . ' - ' . $user->getUsername() . ' == ' . (isset($secanswer) ? "<p>Secquestion: ".htmlentities($secquestion)."<br />Secanswer: $secanswer</p>" : "") . (iconv('GBK', 'UTF-8', $html) ? iconv('GBK', 'UTF-8', $html) : $html);
+            //    $log = new Log('cron', Log::ERROR, $msg);
+            //    $log->save();
+
+                echo "Failed.. \n";
+                echo (iconv('GBK', 'UTF-8', $html) ? iconv('GBK', 'UTF-8', $html) : $html);
+                echo "\n";
+                sendemailAdmin('Dingtie: Error', $msg);
+              }
+            } else {
+              $msg = 'Error when doing post reply. no valid user available';
+            //  $log = new Log('cron', Log::ERROR, $msg);
+            //  $log->save();
+
+              echo "Error: no valid user available";
+              echo "\n";
+              sendemailAdmin('Dingtie: Error', $msg);
+            }
 }
